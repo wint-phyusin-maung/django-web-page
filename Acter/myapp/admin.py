f3a9ca44db99f1bb19a88132deb7267eb87d9df0
wint-phyusin-maung <sshonn687@gmail.com>
2024-04-13 01:06:19 +0630

    add: database and migration

--- Acter/myapp/admin.py
+++ Acter/myapp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import MyTodo
 # Register your models here.
+
+
+admin.site.register(MyTodo)
+
--- Acter/myapp/views.py
+++ Acter/myapp/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render,HttpResponse
+from .models import MyTodo
 
 # Create your views here.
 
 def home(request):
     return render(request, "home.html")
+
+
+def todos(request):
+    todos = MyTodo.objects.all()
+    return render(request,"todos.html", {"todos": todos})
