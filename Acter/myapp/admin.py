3a1d1a91a8fae79f50c71d2507eedac25da80253
wint-phyusin-maung <sshonn687@gmail.com>
2024-04-15 17:43:07 +0630

    add: create teaching model

--- Acter/myapp/admin.py
+++ Acter/myapp/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from .models import MyTodo
+from .models import MyN5Course
 # Register your models here.
 
 
 admin.site.register(MyTodo)
-
+admin.site.register(MyN5Course)
